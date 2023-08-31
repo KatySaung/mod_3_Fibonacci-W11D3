20fad773f46d696c7ce54636105b707dafab4e53
Katherine Yanan Saung <katysaung@gmail.com>
2023-08-31 17:56:39 -0400

    began logic for fibonnaci

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express( );
+// Fibonnaci
+app.get('/fibonnaci/:number',(req,res)=>{
+ let n1= 1 ;
+ let n2= 1;
+let result = n1+n2;
+
+// create while loop to test Fibonnaci with paramNum
+
+})
+
+
+app.listen(3000, ( ) =>{
+    console.log("The listening on port:3000")
+})
+
