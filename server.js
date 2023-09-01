0e30138f1053c4c907088dd51d8ae293d45a466e
Katherine Yanan Saung <katysaung@gmail.com>
2023-09-01 18:03:33 -0400

    completed testing and final push to git

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express( );
 
-// Correct Fibonnaci Numbers [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377]
-// Fibonnaci(0) = 0
-// Fibonnaci(1) = 1
-// result= result-2+result-1 where result>=2
+// Correct List of Fibonnaci Numbers [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377]
+// Fibonnaci first term = 0
+// Fibonnaci second term= 1 
+// Fibonnaci third term = sum of first term and second term
 function fibNum(num){
     let n1= 0;
     let n2 = 1;
     let n3 = 0;
 
-/*fibonnaci logic: if given number[21,34,55] with n1(first number)= 21, n2(second number)=34, and n3(third number)=55 
- let 55< num, let third number = first number+second number( 55=21+34), let first number =second number( 21=34 ) and let second number=third number and (34=55)*/
+/*fibonnaci logic: n=a whole number with 0 as the first term and each term is calculated by Fn-1, Fn-2, Fn=Fn-1+Fn-2 (for n>1) to find the fibonnaci number. Ex: To find the fibonnaci number for the 3rd term, will use 2 for n=2 and Fn=1 */
     while (n3 < num){
         n3 = n1 + n2
         n1 = n2
@@ -27,7 +26,7 @@ app.get('/ ',(req, res) => {
     console.log(fibNum(55), req.params.num)
 });
 //convert result from string to Number
-app.get('/fibonacci/:num', function(req, res) {
+app.get('/fibonacci/:num', (req, res) =>{
     if (fibNum(Number(req.params.num))){
         res.send("Very well, This is fibonnaci.")
     }else{
